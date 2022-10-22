62a80f7288794415ee86599c91a63ae0448334ed
HAYATO SUGAWARA <hy.sugawara03@gmail.com>
2022-10-22 17:00:52 +0900

    課題２完了

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -5,11 +5,13 @@
       <meta http-equiv="X-UA-Compatible" content="IE=edge">
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>論文詳細</title>
+       <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   </head>
   <body>
      <h1>論文詳細</h1>
     @foreach($articles as $article)
     <li><a href="/articles/{{ $article -> id }}">{{ $article -> title }}</a></li>
     @endforeach
+    <button class="btn" onclick="location.href='/articles/create'">新規論文投稿</button>
   </body>
   </html>
